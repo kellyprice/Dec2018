311945a68974770e4c5e0d9d1d5af394253d00f8
Kelly <price_k_r@yahoo.com>
2019-08-12 16:05:37 +0100

    Create snippets.cs

---
+++ snippets.cs
@@ -0,0 +1,24 @@
+string[] roles = { "Admin", "User" };
+
+var ticket = new FormsAuthenticationTicket(
+version: 1,
+name: "e66ef343-f337-4f7e-8032-91e9a1227376",
+issueDate: DateTime.Now,
+expiration: DateTime.Now.AddSeconds(2000),
+isPersistent: false,
+userData: String.Join("|", roles));
+
+var encryptedTicket = FormsAuthentication.Encrypt(ticket);
+var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+
+Response.Cookies.Add(cookie);
+
+//FormsAuthentication.SetAuthCookie(userSysId.ToString(), false);
+
+if (Url.IsLocalUrl(returnurl) && returnurl.Length > 1 && returnurl.StartsWith("/")
+    && !returnurl.StartsWith("//") && !returnurl.StartsWith("/\\"))
+{
+    return Redirect(returnurl);
+}
+
+return RedirectToAction("Index", "Home");
